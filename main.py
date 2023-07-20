f886b39d08f508d055ce2229c01375ada7f786c1
bahare-behzadi <53374314+bahare-behzadi@users.noreply.github.com>
2023-07-20 23:34:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 GREEN = "#9BE8D8"
-FONT = ("time new roman", 13, "normal")
+FONT = ("time new roman", 12, "normal")
+
+
+# ------------------------------ FIND PASSWORD  --------------------------------- #
+def find():
+    web = web_entry.get()
+    try:
+        with open("data.json") as data:
+            temp_data = json.load(data)
+    except FileNotFoundError:
+        messagebox.showinfo(title="warning", message="there is no file")
+    else:
+        if web in temp_data:
+            messagebox.showinfo(title="Your user and password",
+                                message=f"email/username:{temp_data[web]['email/username']}"
+                                        f"\npassword:{temp_data[web]['password']}")
+        else:
+            messagebox.showinfo(title="warning", message="you dont have password for this website")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -16,13 +35,14 @@ def generator():
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     password_list = [choice(letters) for _ in range(randint(8, 10))] + \
-                    [choice(symbols) for _ in range(randint(2, 4))] +\
+                    [choice(symbols) for _ in range(randint(2, 4))] + \
                     [choice(numbers) for _ in range(randint(2, 4))]
     shuffle(password_list)
 
     password = "".join(password_list)
     pass_entry.insert(0, password)
     pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -31,6 +51,12 @@ def save():
     web = web_entry.get()
     user = user_entry.get()
     password = pass_entry.get()
+    new_data = {
+        web: {
+            "email/username": user,
+            "password": password
+        }
+    }
     if web == "":
         messagebox.showinfo(title="warning", message="Website is empty, you can not leave any field empty")
     elif user == "":
@@ -38,11 +64,17 @@ def save():
     elif password == "":
         messagebox.showinfo(title="warning", message="password is empty, you can not leave any field empty")
     else:
-        is_ok = messagebox.askokcancel(title=web, message=f"The data which you entered is:\nEmail/username:"
-                                                              f"{user}\npassword: {password}\nis it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data:
-                data.write(f"{web} | {user} | {password} |\n")
+        try:
+            with open("data.json", "r") as data:
+                temp_data = json.load(data)
+        except FileNotFoundError or temp_data == "":
+            with open("data.json", "w") as data:
+                json.dump(new_data, data, indent=4)
+        else:
+            temp_data.update(new_data)
+            with open("data.json", "w") as data:
+                json.dump(temp_data, data, indent=4)
+        finally:
             web_entry.delete(0, END)
             web_entry.focus()
             user_entry.delete(0, END)
@@ -61,21 +93,23 @@ canvas.create_image(100, 100, image=photo)
 canvas.grid(column=1, row=0)
 web_label = Label(text="Website: ", bg=GREEN, highlightthickness=0, font=FONT)
 web_label.grid(column=0, row=1)
-web_entry = Entry(width=38, font=FONT)
+web_entry = Entry(width=21, font=FONT)
 web_entry.focus()
-web_entry.grid(column=1, row=1, columnspan=2)
+web_entry.grid(column=1, row=1, columnspan=1)
+search_button = Button(text="Search", font=FONT, width=16, command=find)
+search_button.grid(column=2, row=1)
 user_label = Label(text="Email/Username: ", bg=GREEN, highlightthickness=0, font=FONT)
-user_label.grid(column=0, row=2)
+user_label.grid(column=0, row=4)
 user_entry = Entry(width=38, font=FONT)
-user_entry.grid(column=1, row=2, columnspan=2)
+user_entry.grid(column=1, row=4, columnspan=2)
 user_entry.insert(0, "example@google.com")
 pass_label = Label(text="Password: ", bg=GREEN, highlightthickness=0, font=FONT)
-pass_label.grid(column=0, row=3)
+pass_label.grid(column=0, row=7)
 pass_entry = Entry(width=21, font=FONT)
-pass_entry.grid(column=1, row=3, columnspan=1)
+pass_entry.grid(column=1, row=7, columnspan=1)
 pass_button = Button(text="Generate Password", font=FONT, command=generator)
-pass_button.grid(column=2, row=3, columnspan=1)
-add_button = Button(text="Add", width=32, font=FONT, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+pass_button.grid(column=2, row=7, columnspan=1)
+add_button = Button(text="Add", width=38, font=FONT, command=save)
+add_button.grid(column=1, row=9, columnspan=2)
 
 window.mainloop()
